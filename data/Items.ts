7f32ee03843c0de4b16b2f5f7efbfbdfd5ec85ab
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-31 22:46:01 +0700

    Update Story

--- data/Items.ts
+++ data/Items.ts
@@ -3,7 +3,7 @@ export const SlideItems = [
     {
         image: '/images/SliderImage/img1.jpg',
         title: 'Guardians of the Coast',
-        description: 'Giant statues stand tall on rocky cliffs, watching over a hidden city by the sea. Wrapped in mist and mystery, they protect the land like silent guardians. A lone traveler in red looks out, as if calling to the ancient spirits. This place feels like the start of a forgotten legend.'
+        description: 'Giant statues stand on rocky cliffs, guarding a misty city by the sea. A lone traveler in red gazes out, as if calling to ancient spirits. It feels like the beginning of a forgotten legend.'
     },
     {
         image: '/images/SliderImage/img2.jpg',
@@ -16,13 +16,30 @@ export const SlideItems = [
         description: 'A fleet of Viking ships sails through a raging storm, lightning striking in the distance. The sea roars, but the warriors press forward — fearless, ready for conquest.'
     },
     {
+        image: '/images/SliderImage/img5.jpg',
+        title: 'Bound Titan',
+        description: '"Chained between cliffs, a forgotten giant looms over stormy seas. Lightning strikes as a lone warrior watches. No one recalls how the titan was bound—only that if freed, the world will change forever.'
+    },
+    
+    {
         image: '/images/SliderImage/img4.jpg',
         title: 'The Last Stand',
         description: 'In the heart of a frozen forest, a lone knight faces a massive dragon. With a flaming sword and shield, he stands firm. This battle is not just for survival - it is for legend.'
     },
+
     {
-        image: '/images/SliderImage/img5.jpg',
-        title: 'Bound Titan',
-        description: 'Chained between the cliffs, a forgotten giant looms over stormy seas. Lightning cracks the sky as a lone warrior watches from the shore. No one remembers how the titan was captured — only that if the chains break, the world will never be the same.'
+        image: '/images/SliderImage/img8.jpg',
+        title: 'Echoes of the Highlands',
+        description: 'Chained between cliffs, a forgotten giant looms over stormy seas. Lightning strikes as a lone warrior watches. No one recalls how the titan was bound—only that if freed, the world will change forever.'
+    },
+    {
+        image: '/images/SliderImage/img6.jpg',
+        title: 'Valley of the Fallen',
+        description: 'In a dark canyon, undead legions charge with bone giants and winged beasts. The ground shakes as the forgotten rise for one final battle.'
+    },
+    {
+        image: '/images/SliderImage/img7.jpg',
+        title: 'Embers of Farewell',
+        description: 'In a snowy field, warriors stand in silence as a great pyre blazes. Flames rise to the sky, honoring the fallen with a final, sacred goodbye.'
     }
 ]

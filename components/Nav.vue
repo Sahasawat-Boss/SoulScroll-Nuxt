22e5720ae174b0f7363388fd70925da9a542cf06
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-31 16:28:19 +0700

    Seperate Comps

---
+++ components/Nav.vue
@@ -0,0 +1,49 @@
+<template>
+    <header>
+        <div class="logo">VSlide</div>
+        <ul class="menu">
+            <li>Home</li>
+            <li>Blog</li>
+            <li>Info</li>
+        </ul>
+        <div class="search">
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            </svg>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+svg {
+    width: 25px;
+}
+
+header {
+    width: 1200px;
+    max-width: 90%;
+    margin: auto;
+    display: grid;
+    grid-template-columns: 50px 1fr 50px;
+    grid-template-rows: 50px;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    z-index: 100;
+}
+
+header .logo {
+    font-weight: bold;
+}
+
+header .menu {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    font-weight: 500;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,91 @@
-<script setup>
-
-</script>
-
 <template>
     <div>
-        <ImageSlider/>
+        <Nav />
+
+        <div class="slider">
+            <Slider :items="items" :itemActive="itemActive" />
+
+            <div class="arrows">
+                <button @click="prevSlide">
+                    Back </button>
+                        <button @click="nextSlide"> Next</button>
+            </div>
+
+            <Thumbnail :items="items" :itemActive="itemActive" @slideTo="goToSlide" />
+        </div>
     </div>
 </template>
+
+<script setup>
+import { Nav, Thumbnail, Slider } from '#components'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+
+const items = [
+    {
+        image: '/images/SliderImage/img1.png',
+        title: 'Slider 01',
+        description: 'Lorem ipsum dolor sit amet...'
+    },
+    {
+        image: '/images/SliderImage/img2.jpg',
+        title: 'Slider 02',
+        description: 'Lorem ipsum dolor sit amet...'
+    },
+    {
+        image: '/images/SliderImage/img3.jpg',
+        title: 'Slider 03',
+        description: 'Lorem ipsum dolor sit amet...'
+    },
+    {
+        image: '/images/SliderImage/img4.jpg',
+        title: 'Slider 04',
+        description: 'Lorem ipsum dolor sit amet...'
+    },
+    {
+        image: '/images/SliderImage/img5.jpg',
+        title: 'Slider 05',
+        description: 'Lorem ipsum dolor sit amet...'
+    },
+]
+
+const itemActive = ref(0)
+let interval
+
+const showSlider = () => {
+    scrollThumbnailIntoView()
+    clearInterval(interval)
+    interval = setInterval(nextSlide, 5000)
+}
+
+const nextSlide = () => {
+    itemActive.value = (itemActive.value + 1) % items.length
+    showSlider()
+}
+
+const prevSlide = () => {
+    itemActive.value = (itemActive.value - 1 + items.length) % items.length
+    showSlider()
+}
+
+const goToSlide = (index) => {
+    itemActive.value = index
+    showSlider()
+}
+
+const scrollThumbnailIntoView = () => {
+    const activeThumbnail = document.querySelector('.thumbnail .item.active')
+    if (activeThumbnail) {
+        const rect = activeThumbnail.getBoundingClientRect()
+        if (rect.left < 0 || rect.right > window.innerWidth) {
+            activeThumbnail.scrollIntoView({ behavior: 'smooth', inline: 'nearest' })
+        }
+    }
+}
+
+onMounted(() => {
+    interval = setInterval(nextSlide, 5000)
+})
+onBeforeUnmount(() => {
+    clearInterval(interval)
+})
+</script>
